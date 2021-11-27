1829e9a553ef2b41b3970519901729e0296b6ce3
Rex-Imman Robles <reximman03@gmail.com>
2021-11-27 22:09:19 +0800

    Greetings

---
+++ LoopingProg1.py
@@ -0,0 +1,7 @@
+# Create a program that ask for a sentence as input.
+# Display the number of words, vowels and consonants in the input 
+
+# Greetings
+print("Hello! This Program Will Calculate How many Words, Vowels and Consonants are in the input")
+
+
