c316b9a53b9fb86f87510ac365d040380deb9d92
Rex-Imman Robles <reximman03@gmail.com>
2021-11-27 22:14:31 +0800

    word counter and variables

--- LoopingProg1.py
+++ LoopingProg1.py
@@ -7,4 +7,12 @@ print("Hello! This Program Will Calculate How many Words, Vowels and Consonants 
 # ask input from the user
 Sentence = input("Please enter a sentence you like: ")
 
+# word counter
+word_ = Sentence.split()
+
+# variables
+vowels=0
+word=0
+consonants=0
+
 
