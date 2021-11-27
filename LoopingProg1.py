59d6ccddc851a0477158cbf32d352494950f7627
Rex-Imman Robles <reximman03@gmail.com>
2021-11-27 22:12:26 +0800

    ask form the user

--- LoopingProg1.py
+++ LoopingProg1.py
@@ -4,4 +4,7 @@
 # Greetings
 print("Hello! This Program Will Calculate How many Words, Vowels and Consonants are in the input")
 
+# ask input from the user
+Sentence = input("Please enter a sentence you like: ")
+
 
