86e5ddc998a010eeedff9bccd74b0c3ba9a25530
Rex-Imman Robles <reximman03@gmail.com>
2021-11-27 23:50:02 +0800

    converting string into lower case with consonant and vowels counter

--- LoopingProg1.py
+++ LoopingProg1.py
@@ -2,7 +2,7 @@
 # Display the number of words, vowels and consonants in the input 
 
 # Greetings
-print("Hello! This Program Will Calculate How many Words, Vowels and Consonants are in the input")
+print("Hello! This Program Will Calculate How many Words, Vowels and Consonants are in the input sentence")
 
 # ask input from the user
 Sentence = input("Please enter a sentence you like: ")
@@ -15,4 +15,13 @@ vowels=0
 word=0
 consonants=0
 
+#Converting the string to lower case 
+str = Sentence.lower();  
+for i in range(0,len(Sentence)):   
+    # vowel counter
+    if str[i] in ('a','e','i','o','u'):  
+        vowels = vowels + 1;  
+    # consonant counter
+    elif (str[i] >= 'a' and str[i] <= 'z'):  
+        consonants = consonants + 1;    
 
