573540359ce9e62cc20ad398ceaf62fd77c8876e
Rex-Imman Robles <reximman03@gmail.com>
2021-11-29 15:40:05 +0800

    greetings

---
+++ PassProg2.py
@@ -0,0 +1,13 @@
+# Create a program that check if password is valid
+# The password is valid if all criteria are met:
+# a. Greater than 15 letters
+# b. Have at least one capital letter
+# c. Have at least one number
+# d. Have at least one special char (!@#$%^&*()_+ etc)
+# ex.
+# input: P@ssw0rd+P@ssw0rd
+# output: Valid
+# Tip: loop through each character of the input then process it letter by letter
+
+# Greetings
+print("Hello! This Program will you if your password is valid")
