3255aa341fba107e7f463b34f426144ed23eb0a5
Rex-Imman Robles <reximman03@gmail.com>
2021-11-29 17:57:00 +0800

    finish adding condition

--- PassProg2.py
+++ PassProg2.py
@@ -30,4 +30,27 @@ if (len(password) >=15):
         or i == "|" or i == """^""" or i == "{" or i == "}" or i == "[" or i == "]" or i == "_" or i == "~" or i == "," or i == "?" or i == "+"):
             SpclChar += 1
             
+    if (lower >= 1 and upper >= 1 and SpclChar >= 1 and digit >= 1):
+        print("Your password is: ")
+    if (upper < 1 ):
+        print("Oh no ! Your password must contain atleast one capital letter.")
+    if (digit < 1 ):
+        print("Oh no ! Your password must contain atleast one number.")
+    if (SpclChar < 1 ):
+        print("Oh no ! Your password must contain atleast one special character.")
+    if (lower <= 1 and SpclChar <= 1 and digit <= 1 and upper <=1):
+        print("Sorry! The password you have enter is invalid")
+else:
+    for i in password:
+        if (i.islower()):
+            lower +=1
+        if (i.isdigit()):
+            digit +=1
+        if (i.isupper()):
+            upper +=1
+        if (i == "#" or i == "@" or i == "=" or i == "!" or i == "$" or i == "&" or i == "%" or i == "'" or i == "*" or i == "()" or i == "+ " or i == "." \
+        or i == "|" or i == """^""" or i == "{" or i == "}" or i == "[" or i == "]" or i == "_" or i == "~" or i == "," or i == "?" or i == "+"):
+            SpclChar += 1
+            
     
+        
